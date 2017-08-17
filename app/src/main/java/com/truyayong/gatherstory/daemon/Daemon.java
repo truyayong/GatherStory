6b1a871f1d0a66c5e8792394f732bde06acb4474
qiuyayong <804612066@qq.com>
2017-08-17 21:48:42 +0800

    修改异步管理类

--- app/src/main/java/com/truyayong/gatherstory/daemon/Daemon.java
+++ app/src/main/java/com/truyayong/gatherstory/daemon/Daemon.java
@@ -2,27 +2,50 @@ package com.truyayong.gatherstory.daemon;
 
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.os.Looper;
 
 /**
  * Created by jianghurenchenglidazui on 2017/8/15.
  * Email : truyayong@gmail.com
+ * 异步任务管理类
  */
 
 public class Daemon {
 
     private static HandlerThread networkLoadDeamon;
 
-    public static Handler networkHandler = null;
+    public static CustomHandler networkHandler = null;
 
 
-    public synchronized static Handler getNetworkHandler() {
+    public synchronized static CustomHandler getNetworkHandler() {
         if (networkLoadDeamon == null) {
             networkLoadDeamon = new HandlerThread("network_deamon");
             networkLoadDeamon.start();
         }
         if (networkHandler == null) {
-            networkHandler = new Handler(networkLoadDeamon.getLooper());
+            networkHandler = new CustomHandler(networkLoadDeamon.getLooper());
         }
         return networkHandler;
+    }
+
+    //自定义Handler类
+    public static final class CustomHandler extends Handler {
+
+        public CustomHandler(Looper looper) {
+            super(looper);
+        }
+        //确保Runnable被删除
+        public void safeRemoveCallback(final Runnable runnable) {
+            if (Looper.myLooper() == getLooper()) {
+                removeCallbacks(runnable);
+            } else {
+                post(new Runnable() {
+                    @Override
+                    public void run() {
+                        safeRemoveCallback(runnable);
+                    }
+                });
+            }
+        }
     }
 }
